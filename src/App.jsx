806578f374f34e0aa637682f38b70b9514002235
Fidapk <fidapk269@gmail.com>
2025-08-14 15:30:13 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/about";
 import Education from "./components/education";
